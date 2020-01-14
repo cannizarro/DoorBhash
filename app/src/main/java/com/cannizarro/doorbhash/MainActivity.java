850aa49574be802269af5fe224c3e1334917a9d0
cannizarro <asrarul97@gmail.com>
2020-01-15 02:37:50 +0530

    Second

--- app/src/main/java/com/cannizarro/doorbhash/MainActivity.java
+++ app/src/main/java/com/cannizarro/doorbhash/MainActivity.java
@@ -3,12 +3,30 @@ package com.cannizarro.doorbhash;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button add, join;
+    EditText roomName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        add = findViewById(R.id.create);
+        join = findViewById(R.id.join);
+        roomName = findViewById(R.id.roomName);
+
+    }
+
+    public void createRoom(){
+
+    }
+
+    public void joinRoom(){
+
     }
 }
